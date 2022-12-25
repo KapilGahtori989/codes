b9c45c7f6dd9dcf7061ef3285619532e74021414
Kapil <kapilgahtori989@gmail.com>
2022-12-25 09:53:17 +0530

    DPKnapSack01.java

---
+++ DPKnapSack01.java
@@ -0,0 +1,29 @@
+public class DPKnapSack01 {
+    
+    private static int KnapSack(int val[],int wt[],int W, int n){
+        if(W==0 || n==0){
+            return 0;
+        }
+
+        if(wt[n-1]<=W){//valid
+
+            //include
+
+            int ans1=val[n-1]+KnapSack(val, wt, W-wt[n-1], n-1);
+            //exclude
+            int ans2 = KnapSack(val, wt, W, n-1);
+            return Math.max(ans1, ans2);
+        }else{
+            //not valid
+            return KnapSack(val, wt, W, n-1);
+        }
+    }
+    public static void main(String[] args) {
+         int val[] = {15,14,10,45,30};
+         int wt[]={2,5,1,3,4};
+         int w=7;
+         System.out.println(KnapSack(val, wt, w, val.length));
+
+
+    }
+}
